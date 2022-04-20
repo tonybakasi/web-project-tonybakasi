0ea10013475c462c40395a7fcd9b301d68f3f7b0
Tony Bakasi <tonybakasi@gmail.com>
2022-04-20 16:33:52 +0100

    started javascript file again

--- main.js
+++
@@ -1,6 +0,0 @@
-let toggle = document.querySelector("#header.toggle-button");
-let collapse = document.querySelectorAll("#header.collapse");
-
-toggle.addEventListener('click', function(){
-    collapse.forEach(col => col.classList.toggle("collapse-toggle"))
-})
