73f4e774220bea6fcac10d036a53e3213ea4e334
Tony Bakasi <tonybakasi@gmail.com>
2022-04-22 14:53:14 +0100

    added modal method in javascript

--- main.js
+++ main.js
@@ -5,3 +5,23 @@ menu.addEventListener('click', function() {
  menu.classList.toggle('is-active')
  menuLinks.classList.toggle('active');
 })
+
+//Modal items
+const modal = document.getElementById('email-modal');
+const openBtn = document.querySelector('.main-btn');
+const closeBtn = document.querySelector('.close-btn');
+
+//Click events
+openBtn.addEventListener('click', () => {
+    modal.style.display = 'block' ;
+});
+
+closeBtn.addEventListener('click', () => {
+    modal.style.display = 'none';
+});
+
+window.addEventListener('click', (e) => {
+    if(e.target  === modal) {
+        modal.style.display = 'none';
+    }
+})
