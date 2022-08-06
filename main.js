ff309d512f7098e7bb029753650726ffa358c7fa
Tony Bakasi <tonybakasi@gmail.com>
2022-08-06 12:43:48 +0100

    when scrolling, navbar mimises

--- main.js
+++ main.js
@@ -4,4 +4,9 @@ let navbar = document.querySelector('.navbar');
 menu.addEventListener('click', () =>{
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('nav-toggle');
-})
+});
+
+window.onscroll = () => {
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('nav-toggle');
+}
